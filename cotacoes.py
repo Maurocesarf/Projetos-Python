d118f194e99c3b75ddd392d35760fdbc231ac723
Maurocesarf <maurocesarferreira4@gmail.com>
2023-04-09 12:11:13 -0300

    Update cotacoes.py

--- cotacoes.py
+++ cotacoes.py
@@ -7,8 +7,8 @@ cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL
 cotacoes_dic = cotacoes.json()
 text_msg = 'Cotação atualizada: {}\nDólar Americano - R${:.2f}\nEuro - R${:.2f}\nBitcoin - R${:.2f}'.format(datetime.now(),float(cotacoes_dic['USDBRL']['bid']),float(cotacoes_dic['EURBRL']['bid']),float(cotacoes_dic['BTCBRL']['bid']))
 
-account_sid = '********************************'
-auth_token = '*********************************'
+account_sid = '*******************************'
+auth_token = '********************************'
 client = Client(account_sid, auth_token)
 
 number = '+12762925791'
